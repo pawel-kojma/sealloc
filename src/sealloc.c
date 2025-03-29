e2c61e8ca17c8b323b800b3ed6483f62013f27e9
pawel-kojma <pawel.kojma@hotmail.com>
2025-03-29 09:48:16 +0100

    initial free implementation

--- src/sealloc.c
+++ src/sealloc.c
@@ -1,4 +1,7 @@
 #include <sealloc/arena.h>
+#include <sealloc/bin.h>
+#include <sealloc/chunk.h>
+#include <sealloc/logging.h>
 #include <sealloc/run.h>
 #include <sealloc/utils.h>
 
@@ -24,5 +27,52 @@ void *sealloc_malloc(size_t size) {
     }
     return ptr;
   }
-//TODO: implement something to allocate run from some chunk in the arena
+  // TODO: implement something to allocate run from some chunk in the arena
+  run = arena_allocate_run(&main_arena, bin);
+  if (run == NULL) {
+    // EOM
+    return NULL;
+  }
+  bin->runcur = run;
+  ptr = run_allocate(run, bin);
+  if (run_is_depleted(run)) {
+    bin_retire_current_run(bin);
+  }
+  return ptr;
+}
+
+void sealloc_free(void *ptr) {
+  chunk_t *chunk;
+  run_t *run;
+  bin_t *bin;
+  void *run_ptr = NULL;
+  unsigned run_size = 0, reg_size = 0;
+  if (ptr == NULL) return;
+  chunk = arena_get_chunk_from_ptr(&main_arena, ptr);
+  if (chunk == NULL) {
+    // maybe a huge allocation
+  }
+  chunk_get_run_ptr(chunk, ptr, &run_ptr, &run_size, &reg_size);
+  if (run_ptr == NULL) {
+    // If run_ptr is still NULL, then supposed run is not registered in metadata
+    se_error("Invalid call to free()");
+  }
+  if (reg_size > 0) {
+    // small or medium class
+    bin = arena_get_bin_by_reg_size(&main_arena, reg_size);
+  } else {
+    // large class
+    bin = arena_get_bin_by_reg_size(&main_arena, run_size);
+  }
+  run = bin_get_run_by_addr(bin, run_ptr);
+
+  // Might fail because invalid region is being freed
+  run_deallocate(run, bin, ptr);
+
+  if (run_is_freeable(run, bin)) {
+    bin_delete_run(bin, run);
+    if (chunk_deallocate_run(chunk, run)) {
+      arena_deallocate_chunk(&main_arena, chunk);
+    }
+  }
 }
