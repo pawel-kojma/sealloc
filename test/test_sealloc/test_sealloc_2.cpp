84673e29586a876d5212a36deee1541a2d0e2d7a
pawel-kojma <pawel.kojma@hotmail.com>
2025-03-30 23:20:37 +0200

    separate main api tests to different files

---
+++ test/test_sealloc/test_sealloc_2.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <sealloc.h>
+}
+
+TEST(Sealloc, MultiAllocation) {
+  unsigned chunks = 512;
+  void *reg[chunks];
+  for (int i = 0; i < chunks; i++) {
+    reg[i] = sealloc_malloc(16);
+    EXPECT_NE(reg[i], nullptr);
+  }
+  reg[0] = sealloc_malloc(16);
+}
