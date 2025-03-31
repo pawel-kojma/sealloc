584fa2ac173fe46a1f3d91bcfadf0f19ca7b383a
pawel-kojma <pawel.kojma@hotmail.com>
2025-03-31 11:53:15 +0200

    correct test

--- test/test_sealloc/malloc_multi.cpp
+++ test/test_sealloc/malloc_multi.cpp
@@ -19,18 +19,18 @@ TEST(Sealloc, MultiAllocationMedium) {
   unsigned chunks = 512;
   void *reg[chunks];
   for (int i = 0; i < chunks; i++) {
-    reg[i] = sealloc_malloc(16);
+    reg[i] = sealloc_malloc(MEDIUM_CLASS_ALIGNMENT);
     EXPECT_NE(reg[i], nullptr);
   }
-  reg[0] = sealloc_malloc(16);
+  reg[0] = sealloc_malloc(MEDIUM_CLASS_ALIGNMENT);
 }
 
 TEST(Sealloc, MultiAllocationLarge) {
   unsigned chunks = 512;
   void *reg[chunks];
   for (int i = 0; i < chunks; i++) {
-    reg[i] = sealloc_malloc(16);
+    reg[i] = sealloc_malloc(8192);
     EXPECT_NE(reg[i], nullptr);
   }
-  reg[0] = sealloc_malloc(16);
+  reg[0] = sealloc_malloc(8192);
 }
