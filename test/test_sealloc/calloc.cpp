318d2b3edcd652b339ac577a3ad1d71445cf724e
pawel-kojma <pawel.kojma@hotmail.com>
2025-04-02 21:13:03 +0200

    calloc tests

---
+++ test/test_sealloc/calloc.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include <cstring>
+
+extern "C" {
+#include <sealloc/arena.h>
+#include <sealloc/sealloc.h>
+}
+
+class MallocApiTest : public testing::Test {
+ protected:
+  arena_t arena;
+  void SetUp() override { arena_init(&arena); }
+};
+
+TEST_F(MallocApiTest, CallocSimple) {
+  int *reg = (int *)sealloc_calloc(&arena, 2, 16);
+  EXPECT_NE(reg, nullptr);
+  for (int i = 0; i < 32; i++) {
+    reg[i] = i + 1;
+  }
+  for (int i = 0; i < 32; i++) {
+    EXPECT_EQ(reg[i], i + 1);
+  }
+}
+
+TEST_F(MallocApiTest, CallocBadArgs) {
+  int *reg = (int *)sealloc_calloc(&arena, 0, 16);
+  EXPECT_EQ(reg, nullptr);
+  int *reg1 = (int *)sealloc_calloc(&arena, 2, 0);
+  EXPECT_EQ(reg, nullptr);
+}
