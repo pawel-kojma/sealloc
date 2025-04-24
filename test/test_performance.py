b25b81b7ba56c7948c79be575ec7b43557f6c1a2
pawel-kojma <pawel.kojma@hotmail.com>
2025-04-25 00:37:40 +0200

    add more test programs

--- test/test_performance.py
+++ test/test_performance.py
@@ -14,7 +14,32 @@ def prepare_env(dir: Path, prog: Path, library: Path):
                        library.name, str(patched_prog)])
         subprocess.run(["patchelf", "--add-rpath",
                        library.parent, str(patched_prog)])
-    return patched_prog
+
+
+def run_test(tmp_path, output_dir_performance, progs_dir, lib_path, prog_name, cmd, ret_val=0, input_file=None, as_input=False):
+    if input_file:
+        shutil.copyfile(input_file, tmp_path / input_file.name)
+    prepare_env(
+        tmp_path, progs_dir / prog_name, lib_path)
+    if as_input:
+        res = subprocess.run(cmd,
+                             env={"SEALLOC_SEED": "1234"},
+                             capture_output=True,
+                             cwd=tmp_path,
+                             input=input_file.read_bytes()
+                             )
+    else:
+        res = subprocess.run(cmd,
+                             env={"SEALLOC_SEED": "1234"},
+                             capture_output=True,
+                             cwd=tmp_path
+                             )
+    report_file = next(tmp_path.glob(f'*.{prog_name}'), Path())
+    if report_file.exists():
+        (output_dir_performance / prog_name).mkdir(exist_ok=True)
+        shutil.copyfile(
+            report_file, output_dir_performance / prog_name / report_file.name)
+    assert res.returncode == ret_val
 
 
 @pytest.mark.performance
@@ -28,20 +53,10 @@ def prepare_env(dir: Path, prog: Path, library: Path):
 ])
 def test_performance_on_kissat(cmd, output_dir_performance, tmp_path, lib_path, progs_dir):
     input_file = Path("./test/assets/kissat_cnf.out")
-    shutil.copyfile(input_file, tmp_path / input_file.name)
-    prepared_kissat = prepare_env(tmp_path, progs_dir / "kissat", lib_path)
-    res = subprocess.run(cmd + [
-        str(prepared_kissat), "-q", input_file.name],
-        env={"SEALLOC_SEED": "1234"},
-        capture_output=True,
-        cwd=tmp_path
-    )
-    report_file = next(tmp_path.glob('*.kissat'), Path())
-    if report_file.exists():
-        (output_dir_performance / "kissat").mkdir(exist_ok=True)
-        shutil.copyfile(
-            report_file, output_dir_performance / "kissat" / report_file.name)
-    assert res.returncode == 10
+    test_cmd = cmd + [str(tmp_path / "kissat"), "-q", input_file.name]
+    run_test(tmp_path, output_dir_performance, progs_dir, lib_path,
+             "kissat", test_cmd, ret_val=10, input_file=input_file)
+
 
 @pytest.mark.performance
 @pytest.mark.parametrize("cmd", [
@@ -54,20 +69,9 @@ def test_performance_on_kissat(cmd, output_dir_performance, tmp_path, lib_path, 
 ])
 def test_performance_on_espresso(cmd, output_dir_performance, tmp_path, lib_path, progs_dir):
     input_file = Path("./test/assets/largest_espresso")
-    shutil.copyfile(input_file, tmp_path / input_file.name)
-    prepared_espresso = prepare_env(tmp_path, progs_dir / "espresso", lib_path)
-    res = subprocess.run(cmd + [
-        str(prepared_espresso), input_file.name],
-        env={"SEALLOC_SEED": "1234"},
-        capture_output=True,
-        cwd=tmp_path
-    )
-    report_file = next(tmp_path.glob('*.espresso'), Path())
-    if report_file.exists():
-        (output_dir_performance / "espresso").mkdir(exist_ok=True)
-        shutil.copyfile(
-            report_file, output_dir_performance / "espresso" / report_file.name)
-    assert res.returncode == 0
+    test_cmd = cmd + [str(tmp_path / "espresso"), input_file.name]
+    run_test(tmp_path, output_dir_performance, progs_dir, lib_path,
+             "espresso", test_cmd, input_file=input_file)
 
 
 @pytest.mark.performance
@@ -81,21 +85,9 @@ def test_performance_on_espresso(cmd, output_dir_performance, tmp_path, lib_path
 ])
 def test_performance_on_barnes(cmd, output_dir_performance, tmp_path, lib_path, progs_dir):
     input_file = Path("./test/assets/barnes_input")
-    shutil.copyfile(input_file, tmp_path / input_file.name)
-    prepared_barnes = prepare_env(tmp_path, progs_dir / "barnes", lib_path)
-    res = subprocess.run(cmd + [
-        str(prepared_barnes)],
-        env={"SEALLOC_SEED": "1234"},
-        capture_output=True,
-        input=input_file.read_bytes(),
-        cwd=tmp_path
-    )
-    report_file = next(tmp_path.glob('*.barnes'), Path())
-    if report_file.exists():
-        (output_dir_performance / "barnes").mkdir(exist_ok=True)
-        shutil.copyfile(
-            report_file, output_dir_performance / "barnes" / report_file.name)
-    assert res.returncode == 0
+    test_cmd = cmd + [str(tmp_path / "barnes"), input_file.name]
+    run_test(tmp_path, output_dir_performance, progs_dir, lib_path,
+             "barnes", test_cmd, input_file=input_file, as_input=True)
 
 
 @pytest.mark.performance
@@ -108,16 +100,51 @@ def test_performance_on_barnes(cmd, output_dir_performance, tmp_path, lib_path, 
      '--massif-out-file=massif.cfrac', '--pages-as-heap=yes']
 ])
 def test_performance_on_cfrac(cmd, output_dir_performance, tmp_path, lib_path, progs_dir):
-    prepared_cfrac = prepare_env(tmp_path, progs_dir / "cfrac", lib_path)
+    test_cmd = cmd + [str(tmp_path / "cfrac"),
+                      "3707030275882252342412325295197136712092001"]
+    run_test(tmp_path, output_dir_performance, progs_dir, lib_path,
+             "cfrac", test_cmd)
+
+
+@pytest.mark.performance
+@pytest.mark.parametrize("cmd", [
+    ['/usr/bin/time', '-v', '--output=time.ghostscript'],
+    ['/usr/bin/strace', '-c', '--output=strace.ghostscript'],
+    ['/usr/bin/valgrind', '--tool=callgrind',
+     '--callgrind-out-file=callgrind.ghostscript'],
+    ['/usr/bin/valgrind', '--tool=massif',
+     '--massif-out-file=massif.ghostscript', '--pages-as-heap=yes']
+])
+def test_performance_on_ghostscript(cmd, output_dir_performance, tmp_path, lib_path, progs_dir):
+    input_file = Path("./test/assets/intel_sdm_vol1_small.pdf")
+    test_cmd = cmd + [
+        str(tmp_path / "ghostscript"), "-dBATCH", "-dNODISPLAY", input_file.name]
+    run_test(tmp_path, output_dir_performance, progs_dir, lib_path,
+             "ghostscript", test_cmd, input_file=input_file)
+
+
+@pytest.mark.performance
+@pytest.mark.parametrize("cmd", [
+    ['/usr/bin/time', '-v', '--output=time.gcc'],
+    ['/usr/bin/strace', '-c', '--output=strace.gcc'],
+    ['/usr/bin/valgrind', '--tool=callgrind',
+     '--callgrind-out-file=callgrind.gcc'],
+    ['/usr/bin/valgrind', '--tool=massif',
+     '--massif-out-file=massif.gcc', '--pages-as-heap=yes']
+])
+def test_performance_on_gcc(cmd, output_dir_performance, tmp_path, lib_path, progs_dir):
+    """GCC compiling lua interpreter source code."""
+    input_dir = Path("./test/assets/lua/")
+    shutil.copytree(input_dir, tmp_path / "lua")
     res = subprocess.run(cmd + [
-        str(prepared_cfrac), "3707030275882252342412325295197136712092001"],
-        env={"SEALLOC_SEED": "1234"},
+        "/usr/bin/gcc", "-S", "-O2", "-std=c99", "-o", "lua", "onelua.c", "-lm"],
+        env={"SEALLOC_SEED": "1234", "LD_PRELOAD": str(lib_path)},
         capture_output=True,
-        cwd=tmp_path
+        cwd=tmp_path / "lua"
     )
-    report_file = next(tmp_path.glob('*.cfrac'), Path())
-    if report_file.exists():
-        (output_dir_performance / "cfrac").mkdir(exist_ok=True)
+    report_file = next(tmp_path.glob('*.gcc'), None)
+    if report_file:
+        (output_dir_performance / "gcc").mkdir(exist_ok=True)
         shutil.copyfile(
-            report_file, output_dir_performance / "cfrac" / report_file.name)
+            report_file, output_dir_performance / "gcc" / report_file.name)
     assert res.returncode == 0
