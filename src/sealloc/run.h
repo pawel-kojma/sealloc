336624f5ef13103bcea3d939f98c463337aca147
pawel-kojma <pawel.kojma@hotmail.com>
2025-05-24 20:24:51 +0200

    fix size class comments

--- src/sealloc/run.h
+++ src/sealloc/run.h
@@ -20,18 +20,18 @@ struct bin_state;
 typedef struct bin_state bin_t;
 
 typedef enum run_bitmap_state {
-  STATE_FREE = 0,        // region is free
-  STATE_ALLOC = 1,       // region is allocated
-  STATE_ALLOC_FREE = 2,  // region is free but was allocated
+  STATE_FREE = 0,        /*!< Region is free */
+  STATE_ALLOC = 1,       /*!< Region is allocated */
+  STATE_ALLOC_FREE = 2,  /*!< Region is free but was allocated */
 } bstate_t;
 
 typedef struct run_state {
-  ll_entry_t entry;      // Contains run_heap ptr as key
-  uint16_t navail;       // Number of remaining free regions
-  uint16_t nfreed;       // Number of freed regions
-  uint16_t gen;          // Generator
-  uint16_t current_idx;  // Current index
-  uint8_t reg_bitmap[];  // Region bitmap
+  ll_entry_t entry;      /*!< Contains run_heap ptr as key */
+  uint16_t navail;       /*!< Number of remaining free regions */
+  uint16_t nfreed;       /*!< Number of freed regions */
+  uint16_t gen;          /*!< Generator */
+  uint16_t current_idx;  /*!< Current index */
+  uint8_t reg_bitmap[];  /*!< Region bitmap */
 } run_t;
 
 // Allocate region from run
